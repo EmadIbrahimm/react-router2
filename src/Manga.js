9d56109115894f478475c8a6b0d56d4feee68c00
Emad <emad.ibrahim533@gmail.com>
2020-08-12 16:47:17 +0200

    exe done

--- src/Manga.js
+++ src/Manga.js
@@ -4,27 +4,49 @@ class Manga extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: {}
+            name: '',
+            author: '',
         }
     }
     componentDidMount() {
-        const url = `http://localhost:3000/api/${this.props.match.params.id}.json`;
+        const url = `http://localhost:3003/api/${this.props.match.params.id}.json`;
         console.log('url',url)
         fetch(url)
             .then(res => res.json())
             .then(json =>  {
                 console.log('manga', json);
                 this.setState({
-                    name: json.name
+                    name: json.name,
+                    author: json.author,
                 });
-                console.log('state', this.state.name);
+                console.log('name', this.state.name);
             });
             
     }
+    componentDidUpdate() {
+        const url = `http://localhost:3003/api/${this.props.match.params.id}.json`;
+        fetch(url)
+            .then(res => res.json())
+            .then(json =>  {
+                console.log('manga', json);
+                this.setState({
+                    name: json.name,
+                    author: json.author,
+                });
+                console.log('name', this.state.name);
+            });
+
+            
+
+    }
   render() {
+    //   console.log(this.state.name)
     return (
       <div>
-        {this.props.match.params.id}
+        <p> {this.state.author} </p>  
+        {/* {this.props.match.params.id} */}
+        <p>{this.state.name} </p>
+        
       </div>
     );
   }
